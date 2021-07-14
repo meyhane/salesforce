64fcf46dcc021148d8275e5f6ae0c71570e0b17a
meyhane <egemen.yavuz@soprasteria.com>
2021-07-15 00:36:04 +0200

    incomplete

--- service/datasource-service.py
+++ service/datasource-service.py
@@ -6,7 +6,7 @@ import os
 
 import json
 import pytz
-from simple_salesforce import Salesforce,  SalesforceResourceNotFound
+from simple_salesforce import Salesforce, SalesforceError, SalesforceResourceNotFound
 import iso8601
 import logging
 from collections import OrderedDict
@@ -15,72 +15,13 @@ app = Flask(__name__)
 
 logger = None
 
+SF_OBJECTS_CONFIG =  json.loads(os.environ.get("SF_OBJECTS_CONFIG","{}"))
+
 def datetime_format(dt):
     return '%04d' % dt.year + dt.strftime("-%m-%dT%H:%M:%SZ")
 
-
 def to_transit_datetime(dt_int):
     return "~t" + datetime_format(dt_int)
-
-class DataAccess:
-    def __init__(self):
-        self._entities = {}
-
-    def get_entities(self, since, datatype, sf):
-        if self._entities.get(datatype, []) == []:
-            try:
-                fields = getattr(sf, datatype).describe()["fields"]
-            except SalesforceResourceNotFound as e:
-                abort(404)
-            self._entities[datatype] = fields
-        if since is None:
-            return self.get_entitiesdata(datatype, since, sf)
-        else:
-            return [entity for entity in self.get_entitiesdata(datatype, since, sf) if entity["_updated"] > since]
-
-    def get_entitiesdata(self, datatype, since, sf):
-        # if datatype in self._entities:
-        #     if len(self._entities[datatype]) > 0 and self._entities[datatype][0]["_updated"] > "%sZ" % (datetime.now() - timedelta(hours=12)).isoformat():
-        #        return self._entities[datatype]
-        now = datetime.now(pytz.UTC)
-        entities = []
-        end = datetime.now(pytz.UTC)  # we need to use UTC as salesforce API requires this
-
-        if since is None:
-            #fields = getattr(sf, datatype).describe()["fields"]
-            result = [x['Id'] for x in sf.query_all("SELECT Id FROM %s" % (datatype))["records"]]
-        else:
-            start = iso8601.parse_date(since)
-            if getattr(sf, datatype):
-                if end > (start + timedelta(seconds=60)):
-                    result = getattr(sf, datatype).updated(start, end)["ids"]
-                    deleted = getattr(sf, datatype).deleted(start, end)["deletedRecords"]
-                    for e in deleted:
-                        c = OrderedDict({"_id": e["id"]})
-                        # c = {k: v for k, v in c.items() if v}
-                        c.update({"_updated": "%s" % e["deletedDate"]})
-                        c.update({"_deleted": True})
-
-                        entities.append(c)
-        if result:
-            for e in result:
-                c = getattr(sf, datatype).get(e)
-               # c = {k: v for k, v in c.items() if v}
-                c.update({"_id": e})
-                c.update({"_updated": "%s" % c["LastModifiedDate"]})
-
-                for property, value in c.items():
-                    schema = [item for item in self._entities[datatype] if item["name"] == property]
-                    if value and len(schema) > 0 and "type" in schema[0] and schema[0]["type"] == "datetime":
-                        c[property] = to_transit_datetime(parse(value))
-
-
-                entities.append(c)
-
-
-        return entities
-
-data_access_layer = DataAccess()
 
 def get_var(var, scope=None, is_required=False):
     envvar = None
@@ -89,9 +30,74 @@ def get_var(var, scope=None, is_required=False):
     elif (scope is None or scope=="ENV") and var.upper() in os.environ:
         envvar = os.environ.get(var.upper())
     if is_required and envvar is None:
-        abort(400, "cannot read required '%s' in request params or envvar" % (var.upper()))
-    logger.info("Setting %s = %s" % (var, envvar))
+        abort(400, "cannot read required '%s' from request params or envvars" % (var.upper()))
     return envvar
+
+class DataAccess:
+    def sesamify(self, entity, datatype=None):
+        entity.update({"_id": entity["Id"]})
+        entity.update({"_updated": "%s" % entity["LastModifiedDate"]})
+
+        for property, value in entity.items():
+            schema = [item for item in self._entities.get(datatype, []) if item.get("name") == property]
+            if value and len(schema) > 0 and "type" in schema[0] and schema[0]["type"] == "datetime":
+                entity[property] = to_transit_datetime(parse(value))
+
+        return entity
+
+    def __init__(self):
+        self._entities = {}
+
+    def get_entities(self, since, datatype, sf, objectkey=None):
+        if self._entities.get(datatype, []) == []:
+            try:
+                fields = getattr(sf, datatype).describe()["fields"]
+            except SalesforceResourceNotFound as e:
+                abort(404)
+            self._entities[datatype] = fields
+        if objectkey:
+            try:
+                return self.get_entitiesdata(datatype, since, sf, objectkey)
+            except SalesforceResourceNotFound as e:
+                abort(404)
+        if since is None:
+            return self.get_entitiesdata(datatype, since, sf)
+        else:
+            return [entity for entity in self.get_entitiesdata(datatype, since, sf) if entity["_updated"] > since]
+
+    def get_entitiesdata(self, datatype, since, sf, objectkey=None):
+
+        now = datetime.now(pytz.UTC)
+        entities = []
+        end = datetime.now(pytz.UTC)  # we need to use UTC as salesforce API requires this
+        logger.debug(f"objectkey={objectkey}, datatype={datatype}")
+        if objectkey:
+            obj = getattr(sf, datatype).get(objectkey)
+            return [self.sesamify(obj, datatype)]
+        elif since is None:
+            #fields = getattr(sf, datatype).describe()["fields"]
+            result = [x['Id'] for x in sf.query_all("SELECT Id FROM %s" % (datatype))["records"]]
+        else:
+            start = iso8601.parse_date(since)
+            if start < now + timedelta(days=-30):
+                abort(400, "'since' cannot be more than 30 days ago")
+            if getattr(sf, datatype):
+                if end > (start + timedelta(seconds=60)):
+                    result = getattr(sf, datatype).updated(start, end)["ids"]
+                    deleted = getattr(sf, datatype).deleted(start, end)["deletedRecords"]
+                    for e in deleted:
+                        c = OrderedDict({"_id": e["id"]})
+                        c.update({"_updated": "%s" % e["deletedDate"]})
+                        c.update({"_deleted": True})
+                        entities.append(c)
+        if result:
+            for e in result:
+                c = getattr(sf, datatype).get(e)
+                entities.append(self.sesamify(c, datatype))
+        return entities
+
+data_access_layer = DataAccess()
+
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
@@ -115,10 +121,9 @@ def get_sf():
         auth = request.authorization
     else:
         auth =  {
-            "username": get_var("SECURITY_TOKEN", "ENV") + "\\" + get_var("USERNAME", "ENV"), 
+            "username": get_var("SECURITY_TOKEN", "ENV") + "\\" + get_var("USERNAME", "ENV"),
             "password": get_var("PASSWORD", "ENV")
             }
-    logger.debug(str(auth))
     token, username = auth['username'].split("\\", 1)
     password = auth['password']
 
@@ -129,74 +134,157 @@ def get_sf():
         sf = Salesforce(username, password, token)
     return sf
 
-@app.route('/<datatype>', methods=['GET'])
+@app.route('/<datatype>', methods=['GET'], endpoint="get_all")
+@app.route('/<datatype>/<objectkey>', methods=['GET'], endpoint="get_by_id")
+@app.route('/<datatype>/<ext_id_field>/<ext_id>', methods=['GET'], endpoint="get_by_ext_id")
 @requires_auth
-def get_entities(datatype):
-    since = request.args.get('since')
-    sf = get_sf()
-    entities = sorted(data_access_layer.get_entities(since, datatype, sf), key=lambda k: k["_updated"])
-    return Response(json.dumps(entities), mimetype='application/json')
+def get_entities(datatype, objectkey=None, ext_id_field=None, ext_id=None):
+    try:
+        since = request.args.get('since')
+        sf = get_sf()
+        if request.endpoint == "get_by_ext_id":
+            objectkey = f"{ext_id_field}/{ext_id}"
+        entities = sorted(data_access_layer.get_entities(since, datatype, sf, objectkey), key=lambda k: k["_updated"])
+        return Response(json.dumps(entities), mimetype='application/json')
+    except SalesforceError as err:
+        return Response(json.dumps({"resource_name": err.resource_name, "content": err.content, "url": err.url}),
+            mimetype='application/json',
+            status=err.status)
+    except Exception as err:
+        return Response(str(err), mimetype='plain/text', status=500)
 
-@app.route('/<datatype>', methods=['POST'])
+@app.route('/<datatype>', methods=["POST", "PUT", "PATCH", "DELETE"], endpoint = "crud_all")
+@app.route('/<datatype>/<objectkey>', methods=['POST', "PUT", "PATCH", "DELETE"], endpoint="crud_by_id")
+@app.route('/<datatype>/<ext_id_field>/<ext_id>', methods=["POST", "PUT", "PATCH", "DELETE"], endpoint="crud_by_ext_id")
 @requires_auth
-def receiver(datatype):
-    # get entities from request and write each of them to a file
-    entities = request.get_json()
-    sf = get_sf()
-    if getattr(sf, datatype):
-        transform(datatype, entities, sf)# create the response
-    return Response("Thanks!", mimetype='text/plain')
+def receiver(datatype, objectkey=None, ext_id_field=None, ext_id=None):
+    try:
+        entities = request.get_json()
+        sf = get_sf()
+        if request.endpoint == "crud_by_ext_id":
+            objectkey = f"{ext_id_field}/{ext_id}"
+        if getattr(sf, datatype):
+            transform(datatype, entities, sf, operation_in=request.method, objectkey_in=objectkey)
+        return Response("", mimetype='application/json')
+    except SalesforceError as err:
+        logger.exception(err)
+        return Response(json.dumps({"resource_name": err.resource_name, "content": err.content, "url": err.url}),
+            mimetype='application/json',
+            status=err.status)
+    except Exception as err:
+        logger.exception(err)
+        return Response(str(err), mimetype='plain/text', status=500)
 
-def transform(datatype, entities, sf):
+@app.route('/sf/tooling/<path:path>', methods=["GET", "POST", "DELETE"], endpoint="tooling_execute")
+@requires_auth
+def tooling_execute(path):
+    try:
+        sf = get_sf()
+        data = request.get_json()
+        response_json = sf.toolingexecute(
+            path,
+            method=request.method,
+            data=data)
+        if request.method == "GET":
+            return Response(json.dumps(data_access_layer.sesamify(response_json)), mimetype='application/json')
+        else:
+            return Response(json.dumps(response_json), mimetype='application/json')
+    except SalesforceError as err:
+        logger.exception(err)
+        return Response(json.dumps({"resource_name": err.resource_name, "content": err.content, "url": err.url}),
+            mimetype='application/json',
+            status=err.status)
+    except Exception as err:
+        logger.exception(err)
+        return Response(str(err), mimetype='plain/text', status=500)
+
+@app.route('/ValueSet/CustomField/<field_id>', methods=["GET", "POST", "DELETE"], endpoint="custom_valueset")
+@app.route('/ValueSet/GlobalValueSet/<field_id>', methods=["GET", "POST", "DELETE"], endpoint="global_valueset")
+@requires_auth
+def valueset_execute(field_id):
+    try:
+        path = request.path.replace("/ValueSet", "")
+        sf = get_sf()       
+        data = request.get_json()
+        if request.method == "GET":
+            tooling_api_response = sf.toolingexecute(
+                f"sobjects{path}",
+                method=request.method,
+                data=data)
+            response_data = {"path":path, "_id": path}
+            metadata = tooling_api_response.get("Metadata",{})
+            if request.endpoint == "global_valueset":
+                response_data["valueSet"] = metadata.get("customValue")
+                response_data["fullName"] = metadata.get("masterLabel")
+                response_data["sorted"] = metadata.get("sorted")
+            elif request.endpoint == "custom_valueset":
+                response_data["valueSet"] = metadata.get("valueSet",{}).get("valueSetDefinition",{}).get("value",[])
+                response_data["fullName"] = tooling_api_response.get("FullName")
+                response_data["sorted"] = metadata.get("valueSet",{}).get("valueSetDefinition",{}).get("sorted")
+            return Response(json.dumps(response_data), mimetype='application/json')
+        elif request.method == "DELETE":
+            tooling_api_response = sf.toolingexecute(
+                f"sobjects{path}",
+                method=request.method,
+                data=data)
+
+            return Response(json.dumps(response_json), mimetype='application/json')
+    except SalesforceError as err:
+        logger.exception(err)
+        return Response(json.dumps({"resource_name": err.resource_name, "content": err.content, "url": err.url}),
+            mimetype='application/json',
+            status=err.status)
+    except Exception as err:
+        logger.exception(err)
+        return Response(str(err), mimetype='plain/text', status=500)
+
+def transform(datatype, entities, sf, operation_in="POST", objectkey_in=None):
+    def _get_object_key(entity, objectkey_in=None):
+        '''if 'Id' is specified, use 'Id' as key,
+            else pick the first external id field that has a value'''
+        key_field = "Id"
+        key = None
+        if entity.get(key_field):
+            key = entity[key_field]
+        elif datatype in SF_OBJECTS_CONFIG:
+            for k in SF_OBJECTS_CONFIG[datatype]["ordered_key_fields"]:
+                if entity.get(k):
+                    key_field = k
+                    key = f"{key_field}/{entity[key_field]}"
+                    break
+
+        key = key or objectkey_in
+        if not key:
+            abort(500,"cannot figure out the objectkey for %s" % (entity))
+        #remove fields starting with '_'
+        d = []
+        for p in entity.keys():
+            if p.startswith("_") or p in ["Id", key_field]:
+                d.append(p)
+        for p in d:
+            del(entity[p])
+
+        return entity, key
+
     global ids
-    # create output directory
     c = None
     listing = []
-    if not isinstance(entities, (list)):
+    if not isinstance(entities, list):
         listing.append(entities)
     else:
         listing = entities
     for e in listing:
-        app.logger.info("Updateing entity internal id %s of type %s" % (e["_id"], datatype))
-        del e["_id"]
-        if not ("_deleted" in e and e["_deleted"]):
-            if "Id" in e:
-                app.logger.debug("Getting entity %s of type %s" % (e["Id"], datatype))
-                c = getattr(sf, datatype).get(e["Id"])
-            if not c and "sesam_id__c" in e:
-                try:
-                    c = getattr(sf, datatype).get_by_custom_id("sesam_id__c", e["sesam_id__c"])
-                except:
-                    pass
-            if not c:
-                d = []
-                for p in e.keys():
-                    if p.startswith("_"):
-                        d.append(p)
-                for p in d:
-                    del(e[p])
-                if "Id" in e:
-                    del (e["Id"])
-                getattr(sf, datatype).create(e)
-        if "_deleted" in e and e["_deleted"] :
-            if "Id" in e:
-                app.logger.info("Deleting entity %s of type %s" % (e["Id"],datatype))
-                try:
-                    getattr(sf, datatype).delete(c["Id"])
-                except Exception as err:
-                    app.logger.info("Entity %s of type %s does not exist. Ignoring error: %s..." % (e["Id"], datatype, type(err)))
-                    pass
+        operation = "DELETE" if e.get("_deleted", False) or operation_in == "DELETE" else operation_in
+        object, objectkey = _get_object_key(e, objectkey_in)
+
+        app.logger.debug(f"performing {operation} on {datatype}/{objectkey}")
+        if operation == "DELETE":
+            try:
+                getattr(sf, datatype).delete(objectkey)
+            except Exception as err:
+                app.logger.debug(f"{datatype}/{objectkey} received exception of type {type(err).__name__}")
         else:
-            d = []
-            for p in e.keys():
-                if p.startswith("_"):
-                    d.append(p)
-            for p in d:
-                del(e[p])
-            if "Id" in e:
-                del (e["Id"])
-            if c:
-                getattr(sf, datatype).update(c["Id"], e)
+            getattr(sf, datatype).upsert(objectkey, object)
 
 
 if __name__ == '__main__':
